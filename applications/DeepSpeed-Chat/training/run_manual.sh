cb659d55120838b4410831e34de02bb8ec95bfb3
chuanli11 <cl.chuanli@gmail.com>
2023-10-11 14:40:45 -0600

    update run_manual.sh

--- applications/DeepSpeed-Chat/training/run_manual.sh
+++ applications/DeepSpeed-Chat/training/run_manual.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-python3 make_batch.py nodes/29nodes.txt hostfiles/29nodes_1xN --batchsize 1
-# python3 make_batch.py nodes/29nodes.txt hostfiles/29nodes_2xN --batchsize 2
-# python3 make_batch.py nodes/29nodes.txt hostfiles/29nodes_4xN --batchsize 4
-# python3 make_batch.py nodes/29nodes.txt hostfiles/29nodes_8xN --batchsize 8
-# python3 make_batch.py nodes/29nodes.txt hostfiles/29nodes_16xN --batchsize 16
+NODES=all-nodes-v2
 
-
-./run_batch.sh run_opt-350m hostfiles/29nodes_1xN output/29nodes_1xN_opt-350m 1500
-# ./run_batch.sh run_opt-350m hostfiles/29nodes_2xN output/29nodes_2xN_opt-350m 1500
-# ./run_batch.sh run_opt-350m hostfiles/29nodes_4xN output/29nodes_4xN_opt-350m 1500
-# ./run_batch.sh run_opt-350m hostfiles/29nodes_8xN output/29nodes_8xN_opt-350m 1500
-# ./run_batch.sh run_opt-350m hostfiles/29nodes_16xN output/29nodes_16xN_opt-350m 1500
-
-./run_batch.sh run_opt-13b_bs16_zero0 hostfiles/29nodes_1xN output/29nodes_1xN_opt-13b_bs16_zero0 3000
-
-# ./run_batch.sh run_opt-13b_bs16_zero0 hostfiles/29nodes_2xN output/29nodes_2xN_opt-13b_bs16_zero0 3000
-# ./run_batch.sh run_opt-13b_bs16_zero0 hostfiles/29nodes_4xN output/29nodes_4xN_opt-13b_bs16_zero0 3000
-# ./run_batch.sh run_opt-13b_bs16_zero0 hostfiles/29nodes_8xN output/29nodes_8xN_opt-13b_bs16_zero0 3000
-# ./run_batch.sh run_opt-13b_bs16_zero0 hostfiles/29nodes_16xN output/29nodes_16xN_opt-13b_bs16_zero0 3000
-
-
-python3 eval_batch.py output/29nodes_1xN_opt-350m results/29nodes_1xN_opt-350m.csv 600
-# python3 eval_batch.py output/29nodes_2xN_opt-350m results/29nodes_2xN_opt-350m.csv 600
-# python3 eval_batch.py output/29nodes_4xN_opt-350m results/29nodes_4xN_opt-350m.csv 1200
-# python3 eval_batch.py output/29nodes_8xN_opt-350m results/29nodes_8xN_opt-350m.csv 2300
-# python3 eval_batch.py output/29nodes_16xN_opt-350m results/29nodes_16xN_opt-350m.csv 4200
-
-# python3 eval_batch.py output/29nodes_2xN_opt-13b_bs16_zero0 results/29nodes_2xN_opt-13b_bs16_zero0.csv 130
-# python3 eval_batch.py output/29nodes_4xN_opt-13b_bs16_zero0 results/29nodes_4xN_opt-13b_bs16_zero0.csv 260
-# python3 eval_batch.py output/29nodes_8xN_opt-13b_bs16_zero0 results/29nodes_8xN_opt-13b_bs16_zero0.csv 520
-# python3 eval_batch.py output/29nodes_16xN_opt-13b_bs16_zero0 results/29nodes_16xN_opt-13b_bs16_zero0.csv 1000
+for MODEL in "opt-350m" "opt-13b_bs16_zero0"
+do
+        for BATCH in 1 2 4 8 16 32
+        do
+            python3 make_batch.py nodes/${NODES}.txt hostfiles/${NODES}_${BATCH}xN --batchsize ${BATCH}
+            ./run_batch.sh run_${MODEL} hostfiles/${NODES}_${BATCH}xN output/${NODES}_${BATCH}xN_${MODEL} 1500
+            python3 eval_batch.py output/${NODES}_${BATCH}xN_${MODEL} results/${NODES}_${BATCH}xN_${MODEL}.csv 6000
+        done
+done
