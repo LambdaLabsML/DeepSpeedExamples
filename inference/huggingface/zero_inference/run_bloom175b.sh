82a4e62bde7eda1ae57f58f9a2386e1456da1c43
chuanli11 <cl.chuanli@gmail.com>
2023-11-28 16:20:42 -0800

    Add general run scripts

---
+++ inference/huggingface/zero_inference/run_bloom175b.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+export USE_TF=0 
+BASE_LOG_DIR=./experiments/zero_inference/
+MODEL_NAME="bloom"
+FULL_MODEL_NAME="bigscience/${MODEL_NAME}"
+QB=4
+
+. path-to-venv/bin/activate
+
+# zero-inference
+BSZ=1
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
+BSZ=2
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
+BSZ=4
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
+BSZ=8
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
+BSZ=16
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
+BSZ=24
+LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
+mkdir -p  $LOG_DIR
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu.txt 
+deepspeed --include localhost:0 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --gen-len 32 --pin-memory 1 --quant_bit ${QB} --cpu-offload --kv-offload --log-file $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_pin_q${QB}_cpu_kv.txt 
+
--- inference/huggingface/zero_inference/run_model.py
+++ inference/huggingface/zero_inference/run_model.py
@@ -16,31 +16,50 @@ from deepspeed.accelerator import get_accelerator
 import deepspeed.comm as dist
 from accelerate import init_empty_weights
 from timer import timers
-from transformers import (AutoConfig, AutoTokenizer, AutoModelForCausalLM, 
-                          BloomForCausalLM, OPTForCausalLM, LlamaForCausalLM,
-                        )
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    BloomForCausalLM,
+    OPTForCausalLM,
+    LlamaForCausalLM,
+    FalconForCausalLM,
+)
 from transformers.deepspeed import HfDeepSpeedConfig
-from utils import (GB, add_model_hooks, cache_bytes,
-                   get_filename, get_quant_config, hidden_bytes, meta_to_cpu,
-                   model_bytes, write_benchmark_log)
+from utils import (
+    GB,
+    add_model_hooks,
+    cache_bytes,
+    get_filename,
+    get_quant_config,
+    hidden_bytes,
+    meta_to_cpu,
+    model_bytes,
+    write_benchmark_log,
+)
 from packaging import version
 
-assert version.parse(deepspeed.__version__) >= version.parse("0.10.3"), "ZeRO-Inference with weight quantization and kv cache offloading is available only in DeepSpeed 0.10.3+, please upgrade DeepSpeed"
+assert version.parse(deepspeed.__version__) >= version.parse(
+    "0.10.3"
+), "ZeRO-Inference with weight quantization and kv cache offloading is available only in DeepSpeed 0.10.3+, please upgrade DeepSpeed"
+
 
 def get_tokenizer(model_name, config):
     if config.model_type == "opt":
         # opt175b is not available on HF (at this time),
-        # so as a hack we use opt66b which has similar tokenizer. 
+        # so as a hack we use opt66b which has similar tokenizer.
         tokenizer = AutoTokenizer.from_pretrained(
-            model_name.replace("175b", "66b"), 
-            padding_side="left" 
+            model_name.replace("175b", "66b"), padding_side="left"
         )
     else:
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name, return_token_type_ids=False
+        )
 
     tokenizer.pad_token = tokenizer.eos_token
 
     return tokenizer
+
 
 def get_model_config(model_name):
     if "175b" in model_name:
@@ -53,10 +72,11 @@ def get_model_config(model_name):
     else:
         config = AutoConfig.from_pretrained(model_name, trust_remote_code=True)
 
-    if 'bloom' in model_name:
-        config.model_type = 'bloom'
+    if "bloom" in model_name:
+        config.model_type = "bloom"
 
     return config
+
 
 def get_ds_model(
     model_name,
@@ -67,13 +87,12 @@ def get_ds_model(
     bits,
     group_size,
 ):
-
     config = get_model_config(model_name)
     hidden_size = config.hidden_size
     deepspeed.init_distributed("nccl")
     pin_memory = bool(args.pin_memory)
 
-    if getattr(config, 'torch_dtype', None) is None:
+    if getattr(config, "torch_dtype", None) is None:
         dtype = torch.float16
     else:
         dtype = config.torch_dtype
@@ -87,7 +106,7 @@ def get_ds_model(
         },
         "zero_optimization": {
             "stage": 3,
-            "stage3_prefetch_bucket_size": 2 * hidden_size * hidden_size, # 0, 
+            "stage3_prefetch_bucket_size": 2 * hidden_size * hidden_size,  # 0,
             "stage3_param_persistence_threshold": hidden_size,
             "stage3_max_live_parameters": 2 * hidden_size * hidden_size,
         },
@@ -110,7 +129,7 @@ def get_ds_model(
             pin_memory=pin_memory,
             nvme_path=offload_dir,
             buffer_count=5,
-            buffer_size=9 * GB if config.model_type == 'bloom' else 2 * GB,
+            buffer_size=9 * GB if config.model_type == "bloom" else 2 * GB,
         )
         ds_config["aio"] = {
             "block_size": 1048576,
@@ -130,21 +149,28 @@ def get_ds_model(
 
     if config.model_type in ["bloom", "bloom-7b1"]:
         model = BloomForCausalLM.from_pretrained(
-            dummy_weights or model_name, torch_dtype=dtype,
+            dummy_weights or model_name,
+            torch_dtype=dtype,
         )
     elif config.model_type == "opt":
         model = OPTForCausalLM.from_pretrained(
-            dummy_weights or model_name, torch_dtype=dtype,
+            dummy_weights or model_name,
+            torch_dtype=dtype,
         )
     elif config.model_type == "llama":
         model = LlamaForCausalLM.from_pretrained(
-            dummy_weights or model_name, torch_dtype=dtype,
+            dummy_weights or model_name,
+            torch_dtype=dtype,
+        )
+    elif config.model_type == "falcon":
+        model = FalconForCausalLM.from_pretrained(
+            dummy_weights or model_name,
+            torch_dtype=dtype,
         )
     else:
         raise ValueError(f"Unexpected model type: {config.model_type}")
 
     model = model.eval()
-
 
     ds_engine = deepspeed.initialize(model=model, config_params=ds_config)[0]
     ds_engine.module.eval()
@@ -175,7 +201,7 @@ def run_generation(
     loops,
 ):
     # Load tokenizer
-    config = get_model_config(model_name)    
+    config = get_model_config(model_name)
 
     tokenizer = get_tokenizer(model_name, config)
 
@@ -186,14 +212,14 @@ def run_generation(
         if not os.path.exists(filename):
             print("create dummy weights")
             with init_empty_weights():
-                if config.model_type == 'opt':
+                if config.model_type == "opt":
                     model = OPTForCausalLM(config)
                 elif config.model_type in ["bloom", "bloom-7b1"]:
                     model = BloomForCausalLM(config)
                 elif config.model_type == "llama":
                     model = LlamaForCausalLM(config)
                 else:
-                    raise ValueError(f"Unexpected model type: {config.model_type}")                    
+                    raise ValueError(f"Unexpected model type: {config.model_type}")
             model.save_pretrained(
                 filename, state_dict=meta_to_cpu(model.state_dict(), torch.float16)
             )
@@ -218,7 +244,9 @@ def run_generation(
     prompts = ["Paris is the capital city of"] * (batch_size // dist.get_world_size())
 
     def _batch_encode(prompts):
-        input_tokens = tokenizer.batch_encode_plus(prompts, return_tensors="pt", padding="max_length", max_length=prompt_len)
+        input_tokens = tokenizer.batch_encode_plus(
+            prompts, return_tensors="pt", padding="max_length", max_length=prompt_len
+        )
         for t in input_tokens:
             if torch.is_tensor(input_tokens[t]):
                 input_tokens[t] = input_tokens[t].to(torch.cuda.current_device())
@@ -231,14 +259,15 @@ def run_generation(
 
     # print(model, model.config)
 
-
     add_model_hooks(model)
 
     def set_model_stage(model, stage):
         model.stage = stage
 
     # Run
-    print(f"benchmark, prompt_len = {prompt_len}, execute_gen_len = {execute_gen_len}, input_ids.shape = {input_tokens.input_ids.shape}")
+    print(
+        f"benchmark, prompt_len = {prompt_len}, execute_gen_len = {execute_gen_len}, input_ids.shape = {input_tokens.input_ids.shape}"
+    )
 
     generate_kwargs = dict(max_new_tokens=execute_gen_len, do_sample=False)
     prefill_timings = []
@@ -336,30 +365,72 @@ def run_generation(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="facebook/opt-1.3b", help="model name or path; currently only supports OPT and BLOOM models")
-    parser.add_argument("--dummy", action="store_true", help="Use dummy weights for benchmark purposes.")
-    parser.add_argument("--loops", type=int, default=3,  help="Number of token generation iterations")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="facebook/opt-1.3b",
+        help="model name or path; currently only supports OPT and BLOOM models",
+    )
+    parser.add_argument(
+        "--dummy", action="store_true", help="Use dummy weights for benchmark purposes."
+    )
+    parser.add_argument(
+        "--loops", type=int, default=3, help="Number of token generation iterations"
+    )
     parser.add_argument("--batch-size", type=int, default=1)
-    parser.add_argument("--prompt-len", type=int, default=512,  help="prompt length")
-    parser.add_argument("--gen-len", type=int, default=32,  help="number of tokens to generate")
-    parser.add_argument("--local_rank", type=int, help="local rank for distributed inference")
-    parser.add_argument("--pin-memory", type=int, default=0, help="whether to pinned CPU memory for ZeRO offloading")
+    parser.add_argument("--prompt-len", type=int, default=512, help="prompt length")
+    parser.add_argument(
+        "--gen-len", type=int, default=32, help="number of tokens to generate"
+    )
+    parser.add_argument(
+        "--local_rank", type=int, help="local rank for distributed inference"
+    )
+    parser.add_argument(
+        "--pin-memory",
+        type=int,
+        default=0,
+        help="whether to pinned CPU memory for ZeRO offloading",
+    )
     parser.add_argument("--cpu-offload", action="store_true", help="Use cpu offload.")
     parser.add_argument("--disk-offload", action="store_true", help="Use disk offload.")
-    parser.add_argument("--offload-dir", type=str, default="~/offload_dir", help="Directory to store offloaded cache.")
-    parser.add_argument("--kv-offload", action="store_true", help="Use kv cache cpu offloading.")
+    parser.add_argument(
+        "--offload-dir",
+        type=str,
+        default="~/offload_dir",
+        help="Directory to store offloaded cache.",
+    )
+    parser.add_argument(
+        "--kv-offload", action="store_true", help="Use kv cache cpu offloading."
+    )
     parser.add_argument("--log-file", type=str, default="auto", help="log file name")
     parser.add_argument("--verbose", type=int, default=2, help="verbose level")
-    parser.add_argument("--quant_bits", type=int, default=16, help="model weight quantization bits; either 4 or 8")
-    parser.add_argument("--quant_group_size", type=int, default=64, help="model weight quantization group size")
-    parser.add_argument("--pin_kv_cache", action="store_true", help="Allocate kv cache in pinned memory for offloading.")
-    parser.add_argument("--async_kv_offload", action="store_true", help="Using non_blocking copy for kv cache offloading.")
+    parser.add_argument(
+        "--quant_bits",
+        type=int,
+        default=16,
+        help="model weight quantization bits; either 4 or 8",
+    )
+    parser.add_argument(
+        "--quant_group_size",
+        type=int,
+        default=64,
+        help="model weight quantization group size",
+    )
+    parser.add_argument(
+        "--pin_kv_cache",
+        action="store_true",
+        help="Allocate kv cache in pinned memory for offloading.",
+    )
+    parser.add_argument(
+        "--async_kv_offload",
+        action="store_true",
+        help="Using non_blocking copy for kv cache offloading.",
+    )
     args = parser.parse_args()
 
-    deepspeed.init_distributed()    
+    deepspeed.init_distributed()
     num_gpus_per_node = get_accelerator().device_count()
     num_nodes = dist.get_world_size() // num_gpus_per_node
-
 
     run_generation(
         args.model,
@@ -379,5 +450,5 @@ if __name__ == "__main__":
         args.quant_group_size,
         args.pin_kv_cache,
         args.async_kv_offload,
-        args.loops
+        args.loops,
     )
