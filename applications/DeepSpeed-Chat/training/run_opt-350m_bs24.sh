c30928db56c74cdb5f644baffe50595f338b6c7d
root <root@voyage-lambda-headnode01.cm.cluster>
2024-02-23 05:33:49 +0000

    Update run_test

---
+++ applications/DeepSpeed-Chat/training/run_opt-350m_bs24.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+JOB_NAME=${1:-"1xN"}
+HOSTFILE_NAME=${2:-""}
+OUTPUT_PATH=${3:-"output"}
+
+# Step 2
+MODEL_NAME=opt-350m
+STEP_NAME=2
+
+NAME_LOG=${OUTPUT_PATH}/${MODEL_NAME}_${STEP_NAME}_${JOB_NAME}.log
+cat $HOSTFILE_NAME | tee $NAME_LOG
+echo >> $NAME_LOG
+
+first_line=$(head -n 1 "$HOSTFILE_NAME")
+master_addr=$(echo "$first_line" | awk '{print $1}')
+
+deepspeed_path=$(which deepspeed)
+
+source ./setup_env.sh $MODEL_NAME $STEP_NAME && \
+NCCL_DEBUG=INFO PROJECT_PATH=${PROJECT_PATH} $deepspeed_path --hostfile=$HOSTFILE_NAME --master_addr $master_addr $SCRIPT_PATH/main.py \
+   --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
+   --data_split 2,4,4 \
+   --data_output_path $DATA_OUTPUT_PATH \
+   --model_name_or_path $MODEL_PATH \
+   --num_padding_at_beginning 1 \
+   --per_device_train_batch_size 24 \
+   --per_device_eval_batch_size 4 \
+   --max_seq_len 512 \
+   --learning_rate 1e-10 \
+   --weight_decay 0.1 \
+   --num_train_epochs 1000 \
+   --disable_dropout \
+   --gradient_accumulation_steps 1 \
+   --lr_scheduler_type cosine \
+   --num_warmup_steps 0 \
+   --seed 1234 \
+   --zero_stage $ZERO_STAGE \
+   --deepspeed \
+   --max_steps 100 2>&1 | tee -a $NAME_LOG
+
+
+# Fake benchmark
+# source ./setup_env.sh $MODEL_NAME $STEP_NAME && \
+# # echo $NAME_LOG
+# source ~/.bashrc 
+
+# ulimit -n
+# echo $LLLLL
+# hostname
+
+# cat $HOSTFILE_NAME | tee $NAME_LOG
---
+++ applications/DeepSpeed-Chat/training/run_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+NODES=allnodes
+NSLOTS=8
+TIMEOUT=900
+
+# Declare an associative array for throughput values
+declare -A THROUGHPUTS
+THROUGHPUTS[opt-350m_bs24]=500
+THROUGHPUTS[opt-13b_bs16_zero0]=65
+
+# Add more models here as needed
+for MODEL in "opt-350m_bs24" "opt-13b_bs16_zero0" # Add more models to this list as needed
+do
+    # Access the throughput directly from the array using the model as the key
+    THROUGHPUT=${THROUGHPUTS[$MODEL]}
+
+    for BATCH in 1 2 8
+    do
+        # Create hostfiles for this run
+        python3 make_batch.py nodes/${NODES}.txt hostfiles/${NODES}_${BATCH}xN --batchsize ${BATCH} --append slots=${NSLOTS}
+             
+        # Run jobs in batch
+        ./run_batch.sh run_${MODEL} hostfiles/${NODES}_${BATCH}xN output/${NODES}_${BATCH}xN_${MODEL} $TIMEOUT
+        
+        # Collect throughput from log file and evaluate
+        THROUGHPUT_SCALED=$(($THROUGHPUT * $BATCH))   
+        python3 eval_batch.py output/${NODES}_${BATCH}xN_${MODEL} results/${NODES}_${BATCH}xN_${MODEL}.csv $THROUGHPUT_SCALED
+        
+        # Kill potential hanging jobs
+        ./kill_process.sh nodes/${NODES}.txt python
+        sleep 60
+    done
+done
